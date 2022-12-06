bb4e9444c8fad2afcf53abd18fd89169ff686beb
Robert Schlereth <robert.schlereth@gmail.com>
2022-12-06 13:40:51 -0600

    Add numberStudents greater thanor equal to 21

--- comparisons.js
+++ comparisons.js
@@ -37,6 +37,7 @@ console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log("is numberStudents greater than or equal to 21?", numberStudents >= 21);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
